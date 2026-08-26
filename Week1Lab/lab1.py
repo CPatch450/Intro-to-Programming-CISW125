375296f790a14b15657a3485fbc13b03a411ce53
CPatch450 <cpatch@csi.edu>
2026-08-26 14:24:24 -0600

    added lab2

--- Week1Lab/lab1.py
+++ Week1Lab/lab1.py
@@ -3,3 +3,20 @@ print ("hello!", name)
 #input ("Press enter to close")
 age=input("Enter your age: ")
 print ("Your mame is ", name, " and you are ", age," years old")
+
+print ("Just like every other programming language, there are rules, especially for naming variables:")
+rule1="There can not be spaces, you must either leave them out or use underscores"
+rule2="Variables cannot start with a number but can have numbers elsewhere in the name"
+rule3="Most variables usually start with a lowercase letter"
+print(" 1. ", rule1, "\n 2. ", rule2, "\n 3. ", rule3)
+
+fruit="cherries"
+drink="cold soda"
+print (f"For lunch I want {fruit} and a {drink}")
+
+x=5
+y=6
+z=x-y
+print(f"The value of {x} + {y} is {x + y}")
+print(f"The value of {x} - {y} is {z}")
+
---
+++ Week1Lab/lab2.py
@@ -0,0 +1,33 @@
+x=5
+y=6
+z=x//y
+subtraction=x-y
+division=x/y
+multiplication=x*y
+square = x**x
+
+
+print(f"The value of {x} + {y} is {x + y}")
+print(f"The value of {x} - {y} is {z}")
+print(f"Heres some basic math equations:")
+print(f"{x} / {y} = {division} \n {x} x {y} = {multiplication} \n {x} // {y} = {z}")
+print(f"The square of {x} is {square}")
+
+groceryList="apples, bananas, oranges, bread, milk, chicken"
+print(f"I'm getting groceries later. I'm buying {groceryList}")
+snack=input("What snack do you want? ")
+space=" and "
+fullList= groceryList + space + snack
+print(f"Okay I will buy {fullList}")
+
+first=input("What is your first name? ")
+last=input("What is your last name? ")
+print(f"Your full name is {first + last}")
+
+number=input("Enter a number 1-10: ")
+if number > 10:
+    print("Invalid number")
+elif number < 0:
+    print("Invalid number")
+else:
+    print(f"The number you entered was {number}")
