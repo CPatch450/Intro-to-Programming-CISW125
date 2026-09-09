cf8055cf8358ba417734be181bd479acca9c987d
CPatch450 <cpatch@csi.edu>
2026-09-09 14:46:40 -0600

    finished project1

--- Project1/program1.py
+++ Project1/program1.py
@@ -1,1 +1,27 @@
-#Project 1, porgram 1, Ciara Patch
+#Project 1, porgram 1 (Inputs), Ciara Patch
+
+print("Let's play wheel of facts and do some ice breakers!") #Initializing
+name=input("What is your name? ")
+age=input("How old are you? ")
+
+num=input("What's your favorite prime number? ") #Questions for the wheel
+food=input("What's your favorite tropical fruit? ")
+breakfast=input("What's your least favorite breakfast food? ")
+job=input("Would you rather work day shift outside or night shifts inside for a job? ")
+vacation=input("Would you rather canoe down a river in a rain forest or sand ski down a sand dune in a desert? ")
+wheel=float(input("Pick a number between 1 and 5 and I'll spin the fact wheel! "))
+print(f"Your name is {name}! You are {age} years old")
+if wheel > 5 or wheel < 1:
+    print("and you don't know how to read instructions the first time.") #I'm joking
+
+
+if wheel ==1:
+    print(f"and your favorite prime number is {num}")
+elif wheel == 2:
+    print(f"and your favorite tropical fruit is {food}")
+elif wheel == 3:
+    print(f"and your least favorite breakfast food is {breakfast}")
+elif wheel == 4:
+    print(f"if given the choice between working day shifts outside or night shifts inside, you chose {job}")
+elif wheel == 5:
+    print(f"when asked if you would rather canoe down a river in a rain forest or sand ski down a sand dune in a desert, you chose {vacation}")
--- Project1/program2.py
+++ Project1/program2.py
@@ -1,1 +1,26 @@
-#Project 1, porgram 2, Ciara Patch
+#Project 1, porgram 2 (Math), Ciara Patch
+
+print("I recently heard that you took a trip to Brazil! That's so cool!")
+print("That's gotta be a long flight though, and you'll definetely have had some lay overs")
+print("Let's calculate how many miles you travelled the whole trip!")
+car1=float(input("How far is the airport from your house? "))
+flight1=float(input("How many miles was your first flight? "))
+flight2=float(input("How many miles was your second flight? "))
+car2=float(input("How many miles was tha taxi ride between gates? I heard that second airport was massive. "))
+flight3=float(input("How many miles was your final flight to Brazil? " ))
+car3=float(input("How far was your hotel from the airport? "))
+car4=float(input("I heard you also went on that crazy city tour of Rio that was half by car and half by foot. How long was that? "))
+flightBrazilTotal= flight1 + flight2 + flight3
+flightTotal= flightBrazilTotal * 2
+carBrazilTotal= car1 + car2 + car3 + car4
+carTotal= carBrazilTotal * 2 - car4
+
+print("Wow you did a lot of travelling!")
+print(f"You travelled {flightBrazilTotal} miles by plane on the way to Brazil and {flightTotal} total!")
+print(f"You travelled {carBrazilTotal} miles by car the first half of your trip and you travelled {carTotal} miles by car total!")
+
+print("Speaking of of numbers and Brazil, I bet that hotel was expensive")
+cost=float(input("How expensive was the hotel? "))
+stay=float(input("and how many nights did you stay in Brazil again? "))
+nightlyCost = cost / stay
+print(f"Wow! so they were charging {nightlyCost} per night!")
