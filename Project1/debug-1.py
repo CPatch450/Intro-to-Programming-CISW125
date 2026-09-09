4122da63de954edc229d36cd646cab44bbf430ad
CPatch450 <cpatch@csi.edu>
2026-09-09 14:53:20 -0600

    added comment to debug for project1

--- Project1/debug-1.py
+++ Project1/debug-1.py
@@ -5,9 +5,9 @@
 #Ciara Patch, 9/9/2026
 total = 0
 num1 = input("What's the first number? >")
-total = float(num1)
+total = float(num1) #Made sure to convert string input to float for all variables labelled total
 num2 = input("What's the second number? >")
-total = float(num2) + total
+total = float(num2) + total #added num2 to total instead of overwriting it
 num3 = input("What's the third number? >")
-total = float(num3) + total
-print(f"Total is: {total}")
+total = float(num3) + total #added num3 to total instead of overwriting it
+print(f"Total of {num1}, {num2} and {num3} is {total}")
